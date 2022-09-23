7816b645d59325a258458cb84699eb5aa12e60ca
1004jaein <105484114+1004jaein@users.noreply.github.com>
2022-09-23 23:31:48 +0900

    Add files via upload

---
+++ 20223061-lab04/src/DoubleTester.java
@@ -0,0 +1,10 @@
+
+public class DoubleTester {
+	public static void main(String args[]) {
+		double probability = 8.70;
+		int percentage = (int) (Math.round(probability*100));
+		Math.rint(percentage);
+		System.out.println(percentage);
+	
+	}
+}
